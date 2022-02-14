3d8da0b7ffcd3d770752752a71d1b833ba37f963
Nacelio Barbosa <naceliombdev@gmail.com>
2022-02-13 23:22:59 -0300

    finalize template with express, ts and babel

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import express from 'express';
-
+import express from "express";
 
 const app = express();
 
 app.get('/', (request, response) =>{
-    return response.json({ message: 'Hello Worlds' });
+    return response.json({ message: "Hello World" });
 });
 
 app.listen(3333);
